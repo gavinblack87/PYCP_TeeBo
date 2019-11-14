b89d8105b55556d1a22de8e035f158edb0e73bce
cjmcauley <christopher.mcauley@gmail.com>
2019-11-14 15:10:45 +0000

    event dates added to state

--- client/src/containers/HomePageContainer.js
+++ client/src/containers/HomePageContainer.js
@@ -5,7 +5,8 @@ export default class HomePageContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: []
+            items: [], 
+            eventDates: []
         }
     }
     render() {
