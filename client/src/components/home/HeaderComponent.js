c3d7335b8442ea93c175a0838c9b348c294c709e
cjmcauley <christopher.mcauley@gmail.com>
2019-11-14 15:54:14 +0000

    Header styled

--- client/src/components/home/HeaderComponent.js
+++ client/src/components/home/HeaderComponent.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Logo from '../images/tee_bo_logo.png';
+import '../../styling/components/home/HeaderComponent.css'
 
 export default class HeaderComponent extends Component {
 
     render() {
         return(
           <>
-          <div>
+          <div className='header'>
               <img src={Logo} className='logo' alt='Tee Bo logo'></img>
             </div>
           </>
